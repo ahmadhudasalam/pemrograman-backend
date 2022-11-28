6ba1536ff0094b19f10f1c80b65a1c4223a3b2ce
ahmadhudasalam <ahmadhudasalam9@gmail.com>
2022-11-28 10:13:18 +0700

    Task Pertemuan 9

---
+++ task-pertemuan-9/Controllers/FruitController.js
@@ -0,0 +1,41 @@
+const fruits = require('../data/fruits.js');
+
+// function index() {
+//     for (const fruit of fruits) {
+//         console.log(fruit);
+//     }
+// }
+const index = () => {
+    for (const fruit of fruits) {
+        console.log(fruit);
+    }
+};
+
+// function store(name) {
+//     fruits.push(name);
+//     index();
+//  }
+const store = (name) => {
+    fruits.push(name);
+    index();
+}
+
+// function update(position, name) {
+//     fruits[position] = name;
+//     index();
+//  }
+const update = (position, name) => {
+    fruits[position] = name;
+    index();
+}
+
+// function destroy(position) {
+//     fruits.splice(position, 1);
+//     index();
+// }
+const destroy = (position) => {
+    fruits.splice(position, 1);
+    index();
+}
+
+module.exports = { index, store, update, destroy };
