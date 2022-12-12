9657efe5526ca90ff0056ce2a9ab584c109c8a07
ahmadhudasalam <ahmadhudasalam9@gmail.com>
2022-12-12 10:35:08 +0700

    Uninstall body-parser

--- task-pertemuan-11/app.js
+++ task-pertemuan-11/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Menggunakan middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 // Menggunakan routing (router)
 app.use(router);
--- task-pertemuan-11/data/students.js
+++ task-pertemuan-11/data/students.js
@@ -1,7 +1,5 @@
 // TODO 1: Buat data students
-// code here
 const students = ["Ahmad Huda Salam", "Muhammad Faris", "Muhammad Raihan"];
 
 // TODO 2: export data students
-// code here
 module.exports = students;
