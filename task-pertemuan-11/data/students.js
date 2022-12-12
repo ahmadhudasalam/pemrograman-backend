426baa587d5f4d3290addd4785bcd4adb39c111f
ahmadhudasalam <ahmadhudasalam9@gmail.com>
2022-12-12 09:27:00 +0700

    Task Pertemuan 11

---
+++ task-pertemuan-11/data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+const students = ["Ahmad Huda Salam", "Muhammad Faris", "Muhammad Raihan"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
